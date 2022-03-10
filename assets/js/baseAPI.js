78bbb4e706a3fee7eef9613564db852c252cdc75
jzdw4 <2422083801@qq.com>
2022-03-10 17:09:02 +0800

    完成了主页功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -3,4 +3,22 @@ $.ajaxPrefilter(function (options) {
     // console.log(options.url);
     // 在发起真正的 ajax 请求之前,统一拼接请求的根路径
     options.url = 'http://www.liulongbin.top:3007' + options.url;
+
+    // /my开头才需要headers
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token' || '')
+        }
+    }
+
+    // 全局统一挂载 complete 回调函数
+    options.complete = function (res) {
+        console.log(res);
+        if (res.responseJSON.status == 1 && res.responseJSON.message == "身份认证失败！") {
+            // 强制清空 token
+            localStorage.removeItem('token');
+            // 强制跳转到登录页
+            location.href = '/login.html'
+        }
+    }
 })
